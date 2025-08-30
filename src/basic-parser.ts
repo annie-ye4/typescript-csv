96962a6b8f412fb18453f37f469c5173b9e1e31e
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2025-08-30 09:56:47 -0400

    removing headers functionality, updated test file

--- src/basic-parser.ts
+++ src/basic-parser.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as readline from "readline";
 
-export async function* parseCSVStream(path: string, hasHeader = true) {
+export async function* parseCSVStream(path: string) {
   const fileStream = fs.createReadStream(path);
   const rl = readline.createInterface({
     // read line interface to read line by line
@@ -9,27 +9,8 @@ export async function* parseCSVStream(path: string, hasHeader = true) {
     crlfDelay: Infinity, // handle different line endings
   });
 
-  // holding headers for the first line if hasHeader is true
-  let headers: string[] | null = null;
-  let rowCount = 0;
-
-  // asynchronous iteration over each line in the file
   for await (const line of rl) {
     const values = line.split(",").map((v) => v.trim());
-
-    if (hasHeader && !headers) {
-      // if first line and hasHeader is true, set headers
-      headers = values;
-      continue; // do not process as data
-    }
-
-    // constructing an object mapping headers to values
-    // or return values as an array if no headers
-    const data =
-      hasHeader && headers
-        ? Object.fromEntries(headers.map((h, i) => [h, values[i] ?? null]))
-        : values; // validation for header
-
-    yield data;
+    yield values;
   }
 }
--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -3,25 +3,21 @@ import * as path from "path";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
 
-test("parseCSVStream yields objects with headers", async () => {
+test("parseCSVStream yields arrays", async () => {
   const results = [];
   for await (const row of parseCSVStream(PEOPLE_CSV_PATH)) {
     results.push(row);
   }
-  expect(results).toHaveLength(3);
-  expect(results[0]).toEqual({ name: "Alice", age: "23" });
-  expect(results[1]).toEqual({ name: "Bob", age: "thirty" }); // this should not work
-  expect(results[2]).toEqual({ name: "Charlie", age: "25" });
-});
-
-test("parseCSVStream yields arrays without headers", async () => {
-  const results = [];
-  for await (const row of parseCSVStream(PEOPLE_CSV_PATH, false)) {
-    results.push(row);
-  }
-  expect(results).toHaveLength(4);
+  expect(results).toHaveLength(5);
   expect(results[0]).toEqual(["name", "age"]);
   expect(results[1]).toEqual(["Alice", "23"]);
-  expect(results[2]).toEqual(["Bob", "thirty"]); // this should not work
+  expect(results[2]).toEqual(["Bob", "thirty"]); // why does this work? :(
   expect(results[3]).toEqual(["Charlie", "25"]);
+  expect(results[4]).toEqual(["Nim", "22"]);
+});
+
+test("parseCSVStream yields only arrays", async () => {
+  for await (const row of parseCSVStream(PEOPLE_CSV_PATH)) {
+    expect(Array.isArray(row)).toBe(true);
+  }
 });
